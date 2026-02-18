e66a77894d8de3271a1a097d2b11ad88ca183fb4
AkashKumbhar22 <akashkumbhar2912@gmail.com>
2026-02-18 21:01:19 +0530

    Update star.java

--- star.java
+++ star.java
@@ -19,4 +19,44 @@ public class star {
            // System.out.println();        
     }
     
+}// 50. Second Largest: Find the second largest number in an array without sorting it.
+
+import java.util.Scanner;
+
+public class pract{
+
+public static void main(String[] args){
+
+    Scanner sc = new Scanner(System.in);
+
+
+
+int arr[]={1,2,3,4,5,6};
+
+int largest = Integer.MIN_VALUE;
+
+int sec_large = Integer.MIN_VALUE;
+
+
+
+for(int i=0;i<arr.length;i++){
+
+    if(arr[i]>largest){
+
+        largest = arr[i];
+
+    }
+
+    else if(arr[i]>sec_large && arr[i]<largest){
+
+        sec_large = arr[i];
+
+    }
+
+}
+
+System.out.println(sec_large);
+
+}
+
 }
